984d8595ee4e567b6f4a7694edd8b565a143d224
Xavier Coulon <xcoulon@redhat.com>
2020-01-31 17:28:53 +0100

    Add a label mapper to watch and reconcile other resources (#73)
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>
    Co-authored-by: Matous Jobanek <matousjobanek@gmail.com>

---
+++ pkg/controller/label_event_handler.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// MapToOwnerByLabel returns an event handler will convert events on a resource to requests on
+// another resource whose name if found in a given label
+func MapToOwnerByLabel(namespace, label string) handler.EventHandler {
+	return &handler.EnqueueRequestsFromMapFunc{
+		ToRequests: &eventToOwnerByLabelMapper{
+			label:     label,
+			namespace: namespace,
+		},
+	}
+}
+
+var _ handler.Mapper = &eventToOwnerByLabelMapper{}
+
+// eventToOwnerByLabelMapper implementation of an handler mapper which
+// returns a reconcile request for the resource in the given namespace
+// and whose name is in the given label
+type eventToOwnerByLabelMapper struct {
+	namespace string
+	label     string
+}
+
+// Map maps the namespace to a request on the "owner" (or "associated") resource
+// (if the label exists)
+func (m eventToOwnerByLabelMapper) Map(obj handler.MapObject) []reconcile.Request {
+	if name, exists := obj.Meta.GetLabels()[m.label]; exists {
+		return []reconcile.Request{
+			{
+				NamespacedName: types.NamespacedName{
+					Namespace: m.namespace,
+					Name:      name,
+				},
+			},
+		}
+	}
+	// the obj was not a namespace or it did not have the required label.
+	return []reconcile.Request{}
+}
---
+++ pkg/controller/label_event_handler_test.go
@@ -0,0 +1,64 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+func TestLabelMapper(t *testing.T) {
+
+	t.Run("resource with expected label", func(t *testing.T) {
+		// given
+		objMeta := metav1.ObjectMeta{
+			Name: "bar",
+			Labels: map[string]string{
+				"type":     "che",
+				"owner":    "foo",
+				"revision": "123",
+			},
+		}
+		obj := corev1.Namespace{
+			ObjectMeta: objMeta,
+		}
+		// when
+		result := eventToOwnerByLabelMapper{namespace: "ns", label: "owner"}.Map(handler.MapObject{
+			Meta:   &objMeta,
+			Object: &obj,
+		})
+		// then
+		require.Len(t, result, 1)
+		assert.Equal(t, reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Namespace: "ns",
+				Name:      "foo",
+			},
+		}, result[0])
+	})
+
+	t.Run("resource without expected label", func(t *testing.T) {
+		// given
+		objMeta := metav1.ObjectMeta{
+			Name: "bar",
+			Labels: map[string]string{
+				"somethingelse": "foo",
+			},
+		}
+		obj := corev1.Namespace{
+			ObjectMeta: objMeta,
+		}
+		// when
+		result := eventToOwnerByLabelMapper{namespace: "ns", label: "owner"}.Map(handler.MapObject{
+			Meta:   &objMeta,
+			Object: &obj,
+		})
+		// then
+		require.Empty(t, result)
+	})
+}
