9553506735a67611bb72f36c0c138cbe649dd290
Tina Kurian <tkurian@redhat.com>
2020-08-04 12:31:51 -0400

    Load secrets and configmaps (#119)
    * load secret and configmap

---
+++ pkg/configuration/load_configuration.go
@@ -0,0 +1,119 @@
+package configuration
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	errs "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+// LoadFromSecret retrieves an operator secret, loads all keys and values from the secret
+// and stores them in a map. This map is then returned by the function.
+// The function doesn't take into account any default values - this has to be
+// handled while getting the values in the configuration object.
+//
+// resourceKey: is the env var which contains the secret resource name.
+// cl: is the client that should be used to retrieve the secret.
+func LoadFromSecret(resourceKey string, cl client.Client) (map[string]string, error) {
+	var secretData = make(map[string]string)
+
+	// get the secret name
+	secretName := getResourceName(resourceKey)
+	if secretName == "" {
+		return secretData, nil
+	}
+
+	// get the secret
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		return secretData, err
+	}
+
+	secret := &v1.Secret{}
+	namespacedName := types.NamespacedName{Namespace: namespace, Name: secretName}
+	err = cl.Get(context.TODO(), namespacedName, secret)
+	if err != nil {
+		if !errs.IsNotFound(err) {
+			return secretData, err
+		}
+		logf.Log.Info("secret is not found")
+	}
+
+	for key, value := range secret.Data {
+		secretData[key] = string(value)
+	}
+
+	return secretData, nil
+}
+
+// LoadFromConfigMap retrieves the host operator configmap and sets environment
+// variables in order to override default configurations.
+// If no configmap is found, then configuration will use all defaults.
+// Returns error if WATCH_NAMESPACE is not set, if the resource GET request failed
+// (for other reasons apart from isNotFound) and if setting env vars fails.
+//
+// prefix: represents the operator prefix (HOST_OPERATOR/MEMBER_OPERATOR)
+// resourceKey: is the env var which contains the configmap resource name.
+// cl: is the client that should be used to retrieve the configmap.
+func LoadFromConfigMap(prefix, resourceKey string, cl client.Client) error {
+	// get the configMap name
+	configMapName := getResourceName(resourceKey)
+	if configMapName == "" {
+		return nil
+	}
+
+	// get the configMap
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		return err
+	}
+
+	configMap := &v1.ConfigMap{}
+	namespacedName := types.NamespacedName{Namespace: namespace, Name: configMapName}
+	err = cl.Get(context.TODO(), namespacedName, configMap)
+	if err != nil {
+		if !errs.IsNotFound(err) {
+			return err
+		}
+		logf.Log.Info("configmap is not found")
+	}
+
+	// get configMap data and set environment variables
+	for key, value := range configMap.Data {
+		configKey := createOperatorEnvVarKey(prefix, key)
+		err := os.Setenv(configKey, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// getResourceName gets the resource name via env var
+func getResourceName(key string) string {
+	// get the resource name
+	resourceName := os.Getenv(key)
+	if resourceName == "" {
+		logf.Log.Info(key + " is not set. Will not override default configurations")
+		return ""
+	}
+
+	return resourceName
+}
+
+// createOperatorEnvVarKey creates env vars based on resource data.
+// Returns env var key.
+//
+// prefix: represents the operator prefix (HOST_OPERATOR/MEMBER_OPERATOR)
+// key: is the value to convert into an env var key
+func createOperatorEnvVarKey(prefix, key string) string {
+	return prefix + "_" + (strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(key, ".", "_"), "-", "_")))
+}
---
+++ pkg/configuration/load_configuration_test.go
@@ -0,0 +1,238 @@
+package configuration
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/codeready-toolchain/toolchain-common/pkg/test"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestLoadFromConfigMap(t *testing.T) {
+	restore := test.SetEnvVarAndRestore(t, "WATCH_NAMESPACE", "toolchain-member-operator")
+	defer restore()
+
+	t.Run("configMap not found", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarAndRestore(t, "MEMBER_OPERATOR_CONFIG_MAP_NAME", "test-config")
+		defer restore()
+
+		cl := test.NewFakeClient(t)
+
+		// when
+		err := LoadFromConfigMap("MEMBER_OPERATOR", "MEMBER_OPERATOR_CONFIG_MAP_NAME", cl)
+
+		// then
+		require.NoError(t, err)
+	})
+	t.Run("no config name set", func(t *testing.T) {
+		// given
+		data := map[string]string{
+			"super-special-key": "super-special-value",
+		}
+		cl := test.NewFakeClient(t, createConfigMap("test-config", "toolchain-host-operator", data))
+
+		// when
+		err := LoadFromConfigMap("HOST_OPERATOR", "HOST_OPERATOR_CONFIG_MAP_NAME", cl)
+
+		// then
+		require.NoError(t, err)
+
+		// test that the secret was not found since no secret name was set
+		testTest := os.Getenv("HOST_OPERATOR_SUPER_SPECIAL_KEY")
+		assert.Equal(t, "", testTest)
+	})
+	t.Run("cannot get configmap", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarAndRestore(t, "MEMBER_OPERATOR_CONFIG_MAP_NAME", "test-config")
+		defer restore()
+
+		data := map[string]string{
+			"test-key-one": "test-value-one",
+		}
+		cl := test.NewFakeClient(t, createConfigMap("test-config", "toolchain-host-operator", data))
+
+		cl.MockGet = func(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+			return errors.New("oopsie woopsie")
+		}
+
+		// when
+		err := LoadFromConfigMap("MEMBER_OPERATOR", "MEMBER_OPERATOR_CONFIG_MAP_NAME", cl)
+
+		// then
+		require.Error(t, err)
+		assert.Equal(t, "oopsie woopsie", err.Error())
+
+		// test env vars are parsed and created correctly
+		testTest := os.Getenv("MEMBER_OPERATOR_TEST_KEY_ONE")
+		assert.Equal(t, testTest, "")
+	})
+	t.Run("env overwrite", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarsAndRestore(t,
+			test.Env("MEMBER_OPERATOR_CONFIG_MAP_NAME", "test-config"),
+			test.Env("MEMBER_OPERATOR_TEST_KEY", ""))
+		defer restore()
+
+		data := map[string]string{
+			"test-key": "test-value",
+		}
+		cl := test.NewFakeClient(t, createConfigMap("test-config", "toolchain-member-operator", data))
+
+		// when
+		err := LoadFromConfigMap("MEMBER_OPERATOR", "MEMBER_OPERATOR_CONFIG_MAP_NAME", cl)
+
+		// then
+		require.NoError(t, err)
+
+		// test env vars are parsed and created correctly
+		testTest := os.Getenv("MEMBER_OPERATOR_TEST_KEY")
+		assert.Equal(t, testTest, "test-value")
+	})
+}
+
+func TestLoadFromSecret(t *testing.T) {
+	restore := test.SetEnvVarAndRestore(t, "WATCH_NAMESPACE", "toolchain-host-operator")
+	defer restore()
+	t.Run("secret not found", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarAndRestore(t, "HOST_OPERATOR_SECRET_NAME", "test-secret")
+		defer restore()
+
+		cl := test.NewFakeClient(t)
+
+		// when
+		secretData, err := LoadFromSecret("HOST_OPERATOR_SECRET_NAME", cl)
+
+		// then
+		require.NoError(t, err)
+		assert.Empty(t, secretData)
+	})
+	t.Run("no secret name set", func(t *testing.T) {
+		// given
+		data := map[string][]byte{
+			"special.key": []byte("special-value"),
+		}
+		cl := test.NewFakeClient(t, createSecret("test-secret", "toolchain-host-operator", data))
+
+		// when
+		secretData, err := LoadFromSecret("HOST_OPERATOR_SECRET_NAME", cl)
+
+		// then
+		require.NoError(t, err)
+		assert.Empty(t, secretData)
+	})
+	t.Run("cannot get secret", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarAndRestore(t, "HOST_OPERATOR_SECRET_NAME", "test-secret")
+		defer restore()
+
+		data := map[string][]byte{
+			"test.key.secret": []byte("test-value-secret"),
+		}
+		cl := test.NewFakeClient(t, createSecret("test-secret", "toolchain-host-operator", data))
+
+		cl.MockGet = func(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+			return errors.New("oopsie woopsie")
+		}
+
+		// when
+		secretData, err := LoadFromSecret("HOST_OPERATOR_SECRET_NAME", cl)
+
+		// then
+		require.Error(t, err)
+		assert.Equal(t, "oopsie woopsie", err.Error())
+		assert.Empty(t, secretData)
+	})
+	t.Run("env overwrite", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarsAndRestore(t,
+			test.Env("HOST_OPERATOR_SECRET_NAME", "test-secret"),
+			test.Env("HOST_OPERATOR_TEST_KEY", ""))
+		defer restore()
+
+		data := map[string][]byte{
+			"test.key": []byte("test-value"),
+		}
+		cl := test.NewFakeClient(t, createSecret("test-secret", "toolchain-host-operator", data))
+
+		// when
+		secretData, err := LoadFromSecret("HOST_OPERATOR_SECRET_NAME", cl)
+
+		// then
+		require.NoError(t, err)
+
+		// test env vars are parsed and created correctly
+		assert.Equal(t, 1, len(secretData))
+		assert.Equal(t, "test-value", secretData["test.key"])
+	})
+}
+
+func TestNoWatchNamespaceSetWhenLoadingSecret(t *testing.T) {
+	t.Run("no watch namespace", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarAndRestore(t, "HOST_OPERATOR_SECRET_NAME", "test-secret")
+		defer restore()
+
+		data := map[string][]byte{
+			"test.key": []byte("test-value"),
+		}
+		cl := test.NewFakeClient(t, createSecret("test-secret", "toolchain-host-operator", data))
+
+		// when
+		secretData, err := LoadFromSecret("HOST_OPERATOR_SECRET_NAME", cl)
+
+		// then
+		require.Error(t, err)
+		assert.Empty(t, secretData)
+		assert.Equal(t, "WATCH_NAMESPACE must be set", err.Error())
+	})
+}
+
+func TestNoWatchNamespaceSetWhenLoadingConfigMap(t *testing.T) {
+	t.Run("no watch namespace", func(t *testing.T) {
+		// given
+		restore := test.SetEnvVarAndRestore(t, "HOST_OPERATOR_CONFIG_MAP_NAME", "test-config")
+		defer restore()
+
+		data := map[string]string{
+			"test-key": "test-value",
+		}
+		cl := test.NewFakeClient(t, createConfigMap("test-config", "toolchain-host-operator", data))
+
+		// when
+		err := LoadFromConfigMap("HOST_OPERATOR", "HOST_OPERATOR_CONFIG_MAP_NAME", cl)
+
+		// then
+		require.Error(t, err)
+		assert.Equal(t, "WATCH_NAMESPACE must be set", err.Error())
+	})
+}
+
+func createSecret(name, namespace string, data map[string][]byte) *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: data,
+	}
+}
+
+func createConfigMap(name, namespace string, data map[string]string) *v1.ConfigMap {
+	return &v1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: data,
+	}
+}
