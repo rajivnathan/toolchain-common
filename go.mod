6991a8136a6644e9bb40b9b3565894a3533c842b
Rajiv Senthilnathan <rsenthil@redhat.com>
2021-06-24 00:01:22 -0400

    Common test functions for dealing with memberoperatorconfig resources (#191)
    * Add new fields to memberoperatorconfig constructor
    
    * Update api dependency
    
    * update api dependency
    
    * fix lint error
    
    * Update api dependency
    
    * IdP renamed to Idp
    
    * Update dependencies
    
    * Update api dependency

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/codeready-toolchain/toolchain-common
 
 require (
-	github.com/codeready-toolchain/api v0.0.0-20210618084322-d8c216fc8eac
+	github.com/codeready-toolchain/api v0.0.0-20210624035742-cf092ca048fa
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/emicklei/go-restful v2.12.0+incompatible // indirect
 	github.com/go-logr/logr v0.4.0
--- pkg/test/config/memberoperatorconfig.go
+++ pkg/test/config/memberoperatorconfig.go
@@ -26,6 +26,171 @@ func (option *MemberOperatorConfigOptionImpl) addFunction(funcToAdd MemberOperat
 	option.toApply = append(option.toApply, funcToAdd)
 }
 
+type AuthOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func Auth() *AuthOption {
+	o := &AuthOption{
+		MemberOperatorConfigOptionImpl: &MemberOperatorConfigOptionImpl{},
+	}
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Auth = toolchainv1alpha1.AuthConfig{}
+	})
+	return o
+}
+
+func (o AuthOption) Idp(value string) AuthOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Auth.Idp = &value
+	})
+	return o
+}
+
+type AutoscalerOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func Autoscaler() *AutoscalerOption {
+	o := &AutoscalerOption{
+		MemberOperatorConfigOptionImpl: &MemberOperatorConfigOptionImpl{},
+	}
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Autoscaler = toolchainv1alpha1.AutoscalerConfig{}
+	})
+	return o
+}
+
+func (o AutoscalerOption) Deploy(value bool) AutoscalerOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Autoscaler.Deploy = &value
+	})
+	return o
+}
+
+func (o AutoscalerOption) BufferMemory(value string) AutoscalerOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Autoscaler.BufferMemory = &value
+	})
+	return o
+}
+
+func (o AutoscalerOption) BufferReplicas(value int) AutoscalerOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Autoscaler.BufferReplicas = &value
+	})
+	return o
+}
+
+type CheOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func Che() *CheOption {
+	o := &CheOption{
+		MemberOperatorConfigOptionImpl: &MemberOperatorConfigOptionImpl{},
+	}
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che = toolchainv1alpha1.CheConfig{}
+	})
+	return o
+}
+
+func (o CheOption) Required(value bool) CheOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.Required = &value
+	})
+	return o
+}
+
+func (o CheOption) UserDeletionEnabled(value bool) CheOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.UserDeletionEnabled = &value
+	})
+	return o
+}
+
+func (o CheOption) KeycloakRouteName(value string) CheOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.KeycloakRouteName = &value
+	})
+	return o
+}
+
+func (o CheOption) Namespace(value string) CheOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.Namespace = &value
+	})
+	return o
+}
+
+func (o CheOption) RouteName(value string) CheOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.RouteName = &value
+	})
+	return o
+}
+
+type CheSecretOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func (o CheOption) Secret() *CheSecretOption {
+	c := &CheSecretOption{
+		MemberOperatorConfigOptionImpl: o.MemberOperatorConfigOptionImpl,
+	}
+	return c
+}
+
+func (o CheSecretOption) Ref(value string) CheSecretOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.Secret.Ref = &value
+	})
+	return o
+}
+
+func (o CheSecretOption) CheAdminUsernameKey(value string) CheSecretOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.Secret.CheAdminUsernameKey = &value
+	})
+	return o
+}
+
+func (o CheSecretOption) CheAdminPasswordKey(value string) CheSecretOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Che.Secret.CheAdminPasswordKey = &value
+	})
+	return o
+}
+
+type ConsoleOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func Console() *ConsoleOption {
+	o := &ConsoleOption{
+		MemberOperatorConfigOptionImpl: &MemberOperatorConfigOptionImpl{},
+	}
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Console = toolchainv1alpha1.ConsoleConfig{}
+	})
+	return o
+}
+
+func (o ConsoleOption) Namespace(value string) ConsoleOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Console.Namespace = &value
+	})
+	return o
+}
+
+func (o ConsoleOption) RouteName(value string) ConsoleOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Console.RouteName = &value
+	})
+	return o
+}
+
 type MemberStatusOption struct {
 	*MemberOperatorConfigOptionImpl
 }
@@ -40,9 +205,65 @@ func MemberStatus() *MemberStatusOption {
 	return o
 }
 
-func (o MemberStatusOption) RefreshPeriod(refreshPeriod string) MemberStatusOption {
+func (o MemberStatusOption) RefreshPeriod(value string) MemberStatusOption {
 	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
-		config.Spec.MemberStatus.RefreshPeriod = &refreshPeriod
+		config.Spec.MemberStatus.RefreshPeriod = &value
+	})
+	return o
+}
+
+type ToolchainClusterOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func ToolchainCluster() *ToolchainClusterOption {
+	o := &ToolchainClusterOption{
+		MemberOperatorConfigOptionImpl: &MemberOperatorConfigOptionImpl{},
+	}
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.ToolchainCluster = toolchainv1alpha1.ToolchainClusterConfig{}
+	})
+	return o
+}
+
+func (o ToolchainClusterOption) HealthCheckPeriod(value string) ToolchainClusterOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.ToolchainCluster.HealthCheckPeriod = &value
+	})
+	return o
+}
+
+func (o ToolchainClusterOption) HealthCheckTimeout(value string) ToolchainClusterOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.ToolchainCluster.HealthCheckTimeout = &value
+	})
+	return o
+}
+
+type WebhookOption struct {
+	*MemberOperatorConfigOptionImpl
+}
+
+func Webhook() *WebhookOption {
+	o := &WebhookOption{
+		MemberOperatorConfigOptionImpl: &MemberOperatorConfigOptionImpl{},
+	}
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Webhook = toolchainv1alpha1.WebhookConfig{}
+	})
+	return o
+}
+
+func (o WebhookOption) Deploy(value bool) WebhookOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Webhook.Deploy = &value
+	})
+	return o
+}
+
+func (o WebhookOption) Image(value string) WebhookOption {
+	o.addFunction(func(config *toolchainv1alpha1.MemberOperatorConfig) {
+		config.Spec.Webhook.Image = &value
 	})
 	return o
 }
