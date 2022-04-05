dffd15a3f9022b9003779e727ba8cf4e73b8557c
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-04-05 12:57:31 -0400

    Remove MatchEmbeddedSpec

--- pkg/test/useraccount/useraccount_assertion.go
+++ pkg/test/useraccount/useraccount_assertion.go
@@ -73,17 +73,9 @@ func (a *Assertion) HasNoFinalizer() *Assertion {
 	return a
 }
 
-func (a *Assertion) MatchEmbeddedSpec(spec toolchainv1alpha1.UserAccountSpecEmbedded) *Assertion {
-	err := a.loadUaAssertion()
-	require.NoError(a.t, err)
-	assert.Equal(a.t, spec.UserAccountSpecBase, a.userAccount.Spec.UserAccountSpecBase)
-	return a
-}
-
 func (a *Assertion) MatchMasterUserRecord(mur *toolchainv1alpha1.MasterUserRecord, spec toolchainv1alpha1.UserAccountSpecEmbedded) *Assertion {
 	err := a.loadUaAssertion()
 	require.NoError(a.t, err)
-	a.MatchEmbeddedSpec(spec)
 	assert.Equal(a.t, mur.Spec.UserID, a.userAccount.Spec.UserID)
 	assert.Equal(a.t, mur.Spec.Disabled, a.userAccount.Spec.Disabled)
 	assert.Equal(a.t, mur.Spec.OriginalSub, a.userAccount.Spec.OriginalSub)
